295fc9b525b14b5a482433340ecf0bf904d1f07a
Nagy Gergő <n.gergo6@gmail.com>
2012-10-21 19:42:23 +0200

    Restarted the app design in accordance to the new requirements

--- trauma-android/gen/rft/trauma/android/R.java
+++ trauma-android/gen/rft/trauma/android/R.java
@@ -11,27 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int _placemarker=0x7f020000;
-        public static final int exit=0x7f020001;
-        public static final int icon=0x7f020002;
-    }
-    public static final class id {
-        public static final int main_mapView=0x7f060000;
-        public static final int main_menu_exit=0x7f060003;
-        public static final int place_marker_EditText_latitude=0x7f060001;
-        public static final int place_marker_EditText_longitude=0x7f060002;
-    }
-    public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int place_marker=0x7f030001;
-    }
-    public static final class menu {
-        public static final int main_menu=0x7f050000;
+        public static final int icon=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int latitude=0x7f040003;
-        public static final int longitude=0x7f040002;
-        public static final int place_marker=0x7f040001;
+        public static final int app_name=0x7f030000;
     }
 }
