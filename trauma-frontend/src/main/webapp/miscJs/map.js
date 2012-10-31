a7719dcb87c098f4a184d404f6d40f17b70f3971
net.zsolt.net@gmail.com <bg62xr01>
2012-10-31 16:00:57 +0100

    map#1

--- trauma-frontend/src/main/webapp/miscJs/map.js
+++ trauma-frontend/src/main/webapp/miscJs/map.js
@@ -1,8 +1,24 @@
 var map = Class.create({
   initialize: function() {
-    this.name = name;
+   
   },
   init: function(element) {
     
+  	var mapOptions = {
+
+	streetViewControl: false,
+	panControl: false,
+	zoomControl: true,
+	zoom: 14,
+	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	zoomControlOptions: {
+	style: google.maps.ZoomControlStyle.LARGE,
+	position: google.maps.ControlPosition.TOP_RIGHT
+		}	
+      
+    }
+
+    var map = new google.maps.Map(element, mapOptions);
+
   }
 });
