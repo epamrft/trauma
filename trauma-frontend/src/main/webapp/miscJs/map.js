fef155bd91aa982ad23f68b06ed53fc388667881
net.zsolt.net@gmail.com <bg62xr01>
2012-10-31 17:31:47 +0100

    map#5

--- trauma-frontend/src/main/webapp/miscJs/map.js
+++ trauma-frontend/src/main/webapp/miscJs/map.js
@@ -23,9 +23,19 @@ var Map = Class.create({
     }
 
     var map = new google.maps.Map(element, mapOptions);
-	this.addContextMenu(map);
 
-	}
+	var contextMenu=this.addContextMenu(map);
+
+	google.maps.event.addListener(map, 'rightclick', function(mouseEvent){
+	contextMenu.show(mouseEvent.latLng);
+	});
+
+	this.addContextListeners(map,contextMenu);
+
+	},
+
+
+
 
     addContextMenu: function(map){
 
@@ -35,15 +45,33 @@ var Map = Class.create({
   
   	menuItems.push({className:'context_menu_item', eventName:'center_map_click', label:'Center map here'});
   	menuItems.push({className:'context_menu_item', eventName:'add_marker_map', label:'Add marker here'});
-  	contextMenuOptions.menuItems=menuItems;
+  	contextMenuOptions.menuItems=menuItems; 	
 
-  	var contextMenu=new ContextMenu(map, contextMenuOptions);
-  
-  		google.maps.event.addListener(map, 'rightclick', function(mouseEvent){
-		contextMenu.show(mouseEvent.latLng);
+		return new ContextMenu(map, contextMenuOptions);
 
+	},
+
+
+	addContextListeners: function(map,contextMenu){
+
+		google.maps.event.addListener(contextMenu, 'menu_item_selected', function(latLng, eventName){
+		switch(eventName){
+			case 'center_map_click':
+				map.panTo(latLng);
+				break;
+				
+				
+			case 'add_marker_map':
+				
+				console.log("Marker Added.")
+
+				break;
+			
+		}
+		
 	});
 
-  }
+	}
+
 
 });
