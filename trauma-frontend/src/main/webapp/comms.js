896dda90a19133233594905286abb04fc3735b1b
net.zsolt.net@gmail.com <bg62xr01>
2012-11-08 14:42:36 +0100

    ajax2

--- trauma-frontend/src/main/webapp/comms.js
+++ trauma-frontend/src/main/webapp/comms.js
@@ -1,60 +1,60 @@
 var BackendComms = Class.create({
 
-	initialize : function() {
-		var url = 'http://trauma.backend.cloudfoundry.com/markers';
-	},
+  initialize : function() {
+    var url = 'http://trauma.backend.cloudfoundry.com/markers';
+  },
 
 
-	sendMarker : function (marker) {		
-    	
-		new Ajax.Request(this.url,
-  		{
-  			method:'POST',
-  			parameters: 'lan: ' + marker.lan + ', lng: ' + marker.lng +  ', desc: ' + desc,
+  sendMarker : function (marker) {    
+      
+    new Ajax.Request(this.url,
+      {
+        method:'POST',
+        parameters: '{lan: ' + marker.lan + ', lng: ' + marker.lng +  ', desc: ' + desc + '}',
 
-    		onSuccess: function(transport, json){
-      			alert(json ? Object.inspect(json) : "no JSON object");
-    		}
+        onSuccess: function(transport, json){
+            alert(json ? Object.inspect(json) : "no JSON object");
+        }
 
-  		});
+      });
 
-	},
-
-
-	deleteMarker : function (markerId) {
-
-		new Ajax.Request(this.url+'/'+markerId,
-  		{
-  			method:'DELETE',
-
-    		onSuccess: function(transport){
-      			alert("Marker deleted successfully!");
-    		}
-
-  		});
-
-	},
+  },
 
 
-	updateMarker : function (markerId, desc) {
-		
-		new Ajax.Request(this.url+'/'+markerId,
-  		{
-  			method:'EDIT',
-  			parameters: 'desc: ' + desc,
+  deleteMarker : function (markerId) {
 
-    		onSuccess: function(transport){
-      			alert("Marker deleted successfully!");	
-    		}
+    new Ajax.Request(this.url+'/'+markerId,
+      {
+        method:'DELETE',
 
-  		});
+        onSuccess: function(transport){
+            alert("Marker deleted successfully!");
+        }
 
-	},
+      });
+
+  },
 
 
-	getMatkers : function (argument) {
-		
-	}
+  updateMarker : function (markerId, desc) {
+    
+    new Ajax.Request(this.url+'/'+markerId,
+      {
+        method:'EDIT',
+        parameters: '{desc: ' + desc + '}',
+
+        onSuccess: function(transport){
+            alert("Marker deleted successfully!");  
+        }
+
+      });
+
+  },
+
+
+  getMatkers : function (argument) {
+    
+  }
 
 
 });
