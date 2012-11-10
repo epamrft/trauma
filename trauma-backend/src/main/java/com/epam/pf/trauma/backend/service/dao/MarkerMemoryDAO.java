7ab55d4b02b02446ed2dc001fae30960b0f5a5ae
Tamás Soós <tamas.soos.pti@gmail.com>
2012-11-10 16:15:39 +0100

    HttpStatus Codes added

--- trauma-backend/src/main/java/com/epam/pf/trauma/backend/service/dao/MarkerMemoryDAO.java
+++ trauma-backend/src/main/java/com/epam/pf/trauma/backend/service/dao/MarkerMemoryDAO.java
@@ -7,7 +7,9 @@ import java.util.concurrent.ConcurrentMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.epam.pf.trauma.backend.service.domain.CentralPoint;
 import com.epam.pf.trauma.backend.service.domain.Marker;
